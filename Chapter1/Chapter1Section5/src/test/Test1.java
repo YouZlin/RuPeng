3fe6bc16aba5af3d4fd91c09c96bd867647ef3f7
zl <zl@127.0.0.1>
2018-10-10 11:22:32 +0800

    Learn the Chapter1

---
+++ Chapter1/Chapter1Section5/src/test/Test1.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Test1 {
+	/**
+	 * 运行可以选择的主入口<br>
+	 * 项目自动生成<br>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 在Window => Preferences => Gerenal 中进行调整
+		 * first:调整字体
+		 * second:调整脚本为中文
+		 * third:sysout;
+		 */
+		
+		System.out.println("Hello World!");//控制台输出
+		//".;注意符号的半角全角
+		//类名,方法名的大写小要对应,否则报错
+	}
+
+}
---
+++ Chapter1/Chapter1Section6/src/test/Test1.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Test1 {
+	
+	/**
+	 * 运行可以选择的主入口<br>
+	 * 项目自动生成<br>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * 开发工具的配置快捷使用
+		 * first:快捷键学习
+		 * second:代码的自动提示
+		 * third:
+		 */
+		System.out.println("Test1.main()");//控制台输出文本内容
+		System.out.print("Test1.main()");//ln代表换行
+		System.err.println("Test1.main()");//控制台输出文本内容,但是是错误提示,一般显示为红字
+	}
+}
